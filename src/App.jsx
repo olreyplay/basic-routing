710bfa2c034fbb556a2ece8e641efb529da25086
Oleg <126164012+olreyplay@users.noreply.github.com>
2023-07-02 13:29:47 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<AbotPage />} />
+      <Route path="/products" element={<ProductsPage />} />
+    </Routes>
+  );
 };
 
 export default App;
